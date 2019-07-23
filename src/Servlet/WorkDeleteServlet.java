587ef99ebe6ce18e1c4655e8fec0d6ef7c2b49f0
Programmerwcn <ning823095469@126.com>
2019-07-23 21:17:28 +0800

    fixed bugs in filter

--- src/Servlet/WorkDeleteServlet.java
+++ src/Servlet/WorkDeleteServlet.java
@@ -1,6 +1,10 @@
 package Servlet;
 
+import Services.FavoursService;
+import Services.IFileUtilService;
 import Services.PaintingService;
+import Services.ServicesImpl.FavoursServiceImpl;
+import Services.ServicesImpl.FileUtilServiceImpl;
 import Services.ServicesImpl.PaintingServiceImpl;
 import bean.Painting;
 import dao.IUserDAO;
@@ -12,18 +16,30 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import java.awt.*;
 import java.io.IOException;
 import java.io.PrintWriter;
 
 @WebServlet(name = "WorkDeleteServlet", value = "/workDelete")
 public class WorkDeleteServlet extends HttpServlet {
+    private static final String UPLOAD_DIRECTORY = "D:\\好好学习\\好好学习\\卓越软件\\Project-JavaEE-2019Summer\\web\\博物馆图片资源\\其他\\";
+    private static final String UPLOAD_VIDEO_DIC = "D:\\好好学习\\好好学习\\卓越软件\\Project-JavaEE-2019Summer\\web\\博物馆图片资源\\videos\\";
+
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         int paintingID = Integer.parseInt(request.getParameter("artworkID"));
         PaintingService paintingService = new PaintingServiceImpl();
+        FavoursService favoursService = new FavoursServiceImpl();
+        IFileUtilService fileUtilService = new FileUtilServiceImpl();
         response.setContentType("text/json;charset=UTF-8");
         response.setCharacterEncoding("UTF-8");
+        Painting painting = paintingService.getOnePainting(paintingID);
+        String filePath = UPLOAD_DIRECTORY + painting.getImageFileName();
+        String videoPath = UPLOAD_VIDEO_DIC + painting.getVideoPath();
         if (paintingService.delete(paintingID) == 1) {
-
+            String deleteFavour = "DELETE FROM favours WHERE artworkID=" + "'" + paintingID + "'";
+            favoursService.delete(deleteFavour);
+            fileUtilService.deleteFile(filePath);
+            //fileUtilService.deleteFile(videoPath);
             String temp = "{\"type\":\"true\",\"msg\":\"删除成功\"}";
             PrintWriter out = response.getWriter();
             out.println(temp);
